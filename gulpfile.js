6e82ac0e17e8d19ceb037d1374831a6025bde54f
roland.alex <roland.alex@mail.ru>
2020-08-22 16:40:42 +0400

    Добавляем в галп автоматизацию imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,15 @@
 
 var gulp = require("gulp");
 var server = require("browser-sync").create();
+var imagemin = require("gulp-imagemin");
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png}")
+    .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+    ]))
+    .pipe(gulp.dest("source/img"));
+});
 
 gulp.task("server", function () {
   server.init({
