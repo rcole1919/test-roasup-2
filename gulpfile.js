4754a5118c38b7f75a2396e595407c10e26f5c6a
roland.alex <roland.alex@mail.ru>
2020-08-23 00:53:50 +0400

    Косметика

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,19 @@
 var gulp = require("gulp");
 var server = require("browser-sync").create();
 var imagemin = require("gulp-imagemin");
+var webp = require("gulp-webp");
 
 gulp.task("images", function () {
-  return gulp.src("source/img/**/*.{png}")
+  return gulp.src("source/img/**/*.png")
     .pipe(imagemin([
-      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.optipng({optimizationLevel: 7}),
     ]))
+    .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("webp", function () {
+  return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
     .pipe(gulp.dest("source/img"));
 });
 
